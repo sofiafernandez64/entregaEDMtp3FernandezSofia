c42b865bb6c97660ceb06ff50b2eb142cdf92f9f
Alumno <Alumno@EM44706182>
2021-04-25 22:46:29 -0300

    Inicializacion del proyecto Fernandez Sofia

---
+++ src/main/java/ar/edu/unju/edm/Tp3FernandezSofiaApplication.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3FernandezSofiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3FernandezSofiaApplication.class, args);
+	System.out.println("Hola");
+	}
+
+}
